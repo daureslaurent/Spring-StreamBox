c5e10fa6405b1b1c580046fff5ce32921ce992c7
dauresl <lolo>
2026-02-05 20:09:34 +0100

    fix

--- src/main/java/com/lda/streambox/port/StreamBoxInput.java
+++ src/main/java/com/lda/streambox/port/StreamBoxInput.java
@@ -6,7 +6,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 public interface StreamBoxInput<T extends StreamBoxBaseEntity> {
-
     List<T> lockNextBatch(int limit);
 
     void addEvent(T streamBoxEvent);
